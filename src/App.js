06ecfe4a63e2752533867ce08b5e4c4258fc6898
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2019-02-23 11:17:21 +0100

    Added book component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Book from "./book.js";
 import "./App.css";
 
 class App extends Component {
@@ -20,9 +21,16 @@ class App extends Component {
     return (
       <div className="App">
         <div className="search">
-          <input type="text" name="" id="" onChange={this.handleSearchChange} />
+          <input
+            type="text"
+            placeholder="Find a book"
+            name=""
+            id=""
+            onChange={this.handleSearchChange}
+          />
           <button>Search</button>
         </div>
+        <Book />
       </div>
     );
   }
---
+++ src/book.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class book extends Component {
+  render() {
+    return (
+      <div className="book">
+        <div className="book-image" />
+      </div>
+    );
+  }
+}
