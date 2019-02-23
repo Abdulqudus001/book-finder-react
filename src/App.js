f7fd33cec4c0a0a624015ee87112076eafa7b706
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2019-02-23 10:49:26 +0100

    Erased existing template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: ""
+    };
+
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
+
+  handleSearchChange(event) {
+    this.setState({
+      searchValue: event.target.value
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="search">
+          <input type="text" name="" id="" onChange={this.handleSearchChange} />
+          <button>Search</button>
+        </div>
       </div>
     );
   }
